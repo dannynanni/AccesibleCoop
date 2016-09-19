7787bdd005c6b50727620c35c4e16d7ec6ab7c92
tsarachan <tes353@nyu.edu>
2016-09-18 21:03:56 -0400

    Script for changing modes
    Brief script for changing between "fight" and "explore" modes. Not yet hooked into the input system.

---
+++ Prototype - Sub - Phase 2/Assets/Scripts/ModeSwitch.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * 
+ * Use this script to change the submarine's mode between "fight" and "explore."
+ * For this script to work, the abilities for each mode must be on separate gameobjects.
+ * E.g., the gun, the sonar, even the different ways of moving must be on separate objects.
+ * 
+ * Call the SwitchMode() script to change states.
+ * 
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class ModeSwitch : MonoBehaviour {
+
+	GameObject[] fightFeatures;
+	GameObject[] exploreFeatures;
+
+	void Start(){
+		fightFeatures = GameObject.FindGameObjectsWithTag("FightMode");
+		exploreFeatures = GameObject.FindGameObjectsWithTag("ExploreMode");
+	}
+
+	public void SwitchMode(){
+		ReverseState(fightFeatures);
+		ReverseState(exploreFeatures);
+	}
+
+	void ReverseState(GameObject[] featuresToChange){
+		foreach (GameObject feature in featuresToChange){
+			feature.SetActive(!feature.activeInHierarchy);
+		}
+	}
+}
