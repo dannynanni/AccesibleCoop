388df8df20cd2c820c14bca84c101f626dd6c749
tsarachan <tes353@nyu.edu>
2016-09-20 12:48:24 -0400

    Captain can power up fight abilities
    CaptainPowerUp now affects fight mode abilities in addition to explore mode abilities.

--- Prototype - Sub - Phase 2/Assets/Scripts/CaptainPowerUp.cs
+++ Prototype - Sub - Phase 2/Assets/Scripts/CaptainPowerUp.cs
@@ -3,20 +3,33 @@ using System.Collections;
 
 public class CaptainPowerUp : MonoBehaviour {
 
+	Transform captain;
 	protected GameObject[] explorePowers;
+	protected GameObject[] fightPowers;
 	protected const string EXPLORE_TAG = "ExploreMode";
+	protected const string FIGHT_TAG = "FightMode";
 	public float powerUpAngle = 10.0f; //how close the captain has to match someone else's direction to power them up
 
 	protected void Start(){
+		captain = transform.parent;
 		explorePowers = GameObject.FindGameObjectsWithTag(EXPLORE_TAG);
+		fightPowers = GameObject.FindGameObjectsWithTag(FIGHT_TAG);
 	}
 
 	protected void Update(){
 		foreach (GameObject crewmember in explorePowers){
-			if (Mathf.Abs(transform.eulerAngles.z - crewmember.transform.eulerAngles.z) <= powerUpAngle){
+			if (Mathf.Abs(captain.eulerAngles.z - crewmember.transform.eulerAngles.z) <= powerUpAngle){
 				crewmember.GetComponent<ExplorePower>().PoweredUp = true;
 			} else {
 				crewmember.GetComponent<ExplorePower>().PoweredUp = false;
+			}
+		}
+
+		foreach (GameObject crewmember in fightPowers){
+			if (Mathf.Abs(captain.eulerAngles.z - crewmember.transform.eulerAngles.z) <= powerUpAngle){
+				crewmember.GetComponent<FightPower>().PoweredUp = true;
+			} else {
+				crewmember.GetComponent<FightPower>().PoweredUp = false;
 			}
 		}
 	}
