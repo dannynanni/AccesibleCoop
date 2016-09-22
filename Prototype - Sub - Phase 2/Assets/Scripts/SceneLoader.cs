2c3dfe64b015f8bd18ccce4410a86208784ba3c0
tsarachan <tes353@nyu.edu>
2016-09-22 17:03:29 -0400

    Additive loading of level
    Added script to load level additively.

---
+++ Prototype - Sub - Phase 2/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour {
+
+	void Awake(){
+		SceneManager.LoadScene("Test Level 1", LoadSceneMode.Additive);
+	}
+}
