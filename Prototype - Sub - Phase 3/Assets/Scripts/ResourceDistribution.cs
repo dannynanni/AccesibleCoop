260b2d2c3053fdbaa5f753ac032fb3fff855e1f3
tsarachan <tes353@nyu.edu>
2016-09-24 21:21:07 -0400

    Selection movement delay
    Added slight delay to movement of captain's resource-refilling marker, to make it easier to control.

--- Prototype - Sub - Phase 3/Assets/Scripts/ResourceDistribution.cs
+++ Prototype - Sub - Phase 3/Assets/Scripts/ResourceDistribution.cs
@@ -9,6 +9,9 @@
 		public PlayerAbility.ResourceUse[] playerAbilities;
 		private Image refillGauge;
 		private RectTransform selectionMarker;
+		public float selectDelay = 0.1f; //the selection marker has to wait this long to move
+		private float selectTimer = 0.0f;
+		private bool waitForNextSelect = false;
 
 		private int selectedAbility = 0;
 		public int SelectedAbility{
@@ -27,6 +30,8 @@
 		public float refillRate = 0.01f; //how fast the submarine's power reserve naturally refills
 		public float dispenseRate = 0.5f; //how fast power flows from the reserve to other player abilities
 		private bool dispensing = false;
+		public float toppedOff = Mathf.Epsilon; //the amount, out of 100, at which the refill stops
+
 
 		private void Start(){
 			refillGauge = GameObject.Find("Refill gauge").GetComponent<Image>();
@@ -38,18 +43,35 @@
 			refillGauge.fillAmount = CurrentResource/resourceMax;
 
 			if (dispensing) { DistributeResource(); }
-		}
 
-		public void ChangeSelectedAbility(float leftRight){
-			if (leftRight < 0.0f){
-				SelectedAbility--;
-			} else if (leftRight > 0.0f){
-				SelectedAbility++;
+			//prevents the selection marker from moving every frame, so that it is controllable
+			if (waitForNextSelect){
+				waitForNextSelect = DelaySelectionInput();
 			}
-
-			selectionMarker.anchoredPosition = MoveSelectionMarker();
 		}
 
+		/// <summary>
+		/// Move between the three ability resource gauges, to decide who to refill.
+		/// </summary>
+		/// <param name="leftRight">The horizontal axis value from the left thumbstick.</param>
+		public void ChangeSelectedAbility(float leftRight){
+			if (!waitForNextSelect){
+				if (leftRight < 0.0f){
+					SelectedAbility--;
+				} else if (leftRight > 0.0f){
+					SelectedAbility++;
+				}
+
+				selectionMarker.anchoredPosition = MoveSelectionMarker();
+				waitForNextSelect = true;
+			}
+		}
+
+		/// <summary>
+		/// Provides the locations for the selection marker as it moves. This location
+		/// is based on the anchored position and assumes the anchor is at the lower-left corner.
+		/// </summary>
+		/// <returns>The (x, y) coordinates the selection marker should go to.</returns>
 		private Vector2 MoveSelectionMarker(){
 			Vector2 leftPos = new Vector2(69.5f, 292.0f);
 			Vector2 centerPos = new Vector2(138.0f, 292.0f);
@@ -71,7 +93,22 @@
 					break;
 			}
 
-			return temp; //this should never be (0.0f, 0.0f) at this point; if it was, there was a problem
+			return temp; //this should never be (0.0f, 0.0f) at this point; if it is, there was a problem
+		}
+
+		/// <summary>
+		/// Determines whether enough time has passed to allow the selection marker to move again.
+		/// </summary>
+		/// <returns><c>true</c>, if enough time has passed, <c>false</c> otherwise.</returns>
+		private bool DelaySelectionInput(){
+			selectTimer += Time.deltaTime;
+
+			if (selectTimer >= selectDelay){
+				selectTimer = 0.0f;
+				return false;
+			}
+
+			return true;
 		}
 
 		public void Button(bool pressed){
@@ -81,11 +118,14 @@
 				dispensing = false;
 			}
 		}
-
+			
+		/// <summary>
+		/// Transfers resources from the captain's gauge to the chosen gauge.
+		/// </summary>
 		private void DistributeResource(){
 			if (CurrentResource >= dispenseRate){ //make sure the captain has enough to distribute . . .
 				//. . . and don't distribute to someone who's already at full
-				if (100.0f - playerAbilities[SelectedAbility].CurrentResource > Mathf.Epsilon){
+				if (100.0f - playerAbilities[SelectedAbility].CurrentResource > toppedOff){
 					playerAbilities[SelectedAbility].CurrentResource += dispenseRate;
 					CurrentResource -= dispenseRate;
 				}
