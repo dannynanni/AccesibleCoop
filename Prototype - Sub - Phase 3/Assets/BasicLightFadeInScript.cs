28c5529b7384a1420cb0f2fda5bc25e99f1723bc
Chris Chung <christopher0chung@gmail.com>
2016-09-27 13:02:31 -0400

    Particle effects for explosions. Not yet a prefab.

---
+++ Prototype - Sub - Phase 3/Assets/BasicLightFadeInScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicLightFadeInScript : MonoBehaviour {
+
+    private SpriteRenderer SR;
+    private float SRAlphaBase;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        SR = GetComponent<SpriteRenderer>();
+        SRAlphaBase = SR.color.a;
+        SR.color = new Color (255, 255, 255, 0);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        timer += Time.deltaTime;
+        if (timer > 2)
+        {
+            SR.color = (Color32.Lerp(SR.color, new Color(255, 255, 255, SRAlphaBase), .05f));
+        }
+
+    }
+}
