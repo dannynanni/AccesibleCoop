7db570e8ae9cdd999085ef3b6305546efc6f1447
tsarachan <tes353@nyu.edu>
2016-09-27 15:42:39 -0400

    Intermittent chase enemy
    New prefab: enemy that swims toward player, pauses, swims toward player again, etc.

--- Prototype - Sub - Phase 3/Assets/Scripts/EnemyBehavior.cs
+++ Prototype - Sub - Phase 3/Assets/Scripts/EnemyBehavior.cs
@@ -1,81 +1,84 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿namespace EnemyBehaviors
+{
+	using UnityEngine;
+	using System.Collections;
 
-public class EnemyBehavior : MonoBehaviour {
+	public class EnemyBehavior : MonoBehaviour {
 
-	protected Rigidbody rigidBody;
-	public float turnRate = 2.0f; //turn rate in degrees/second
-	public float speed = 1.0f;	//maximum forward speed
+		protected Rigidbody rigidBody;
+		public float turnRate = 2.0f; //turn rate in degrees/second
+		public float speed = 1.0f;	//maximum forward speed
 
-	public float spotPlayerDistance = 10.0f; //how far away the enemy will see the player and respond
-	protected Transform player;
-	protected const string PLAYER_NAME = "Players";
+		public float spotPlayerDistance = 10.0f; //how far away the enemy will see the player and respond
+		protected Transform player;
+		protected const string PLAYER_NAME = "Players";
 
-	protected const string PLAYER_WEAPON_NAME = "LightCutout";
-	protected PlayerAbility.BasicLightWeapon playerWeapon;
+		protected const string PLAYER_WEAPON_NAME = "LightCutout";
+		protected PlayerAbility.BasicLightWeapon playerWeapon;
 
-	public float health = 100.0f;
+		public float health = 100.0f;
 
-	protected virtual void Start(){
-		rigidBody = GetComponent<Rigidbody>();
-		player = GameObject.Find(PLAYER_NAME).transform;
-		playerWeapon = GameObject.Find(PLAYER_WEAPON_NAME).GetComponent<PlayerAbility.BasicLightWeapon>();
-	}
-
-	protected virtual void FixedUpdate(){
-		if (Vector3.Distance(player.position, transform.position) <= spotPlayerDistance){
-			ChasePlayer();
+		protected virtual void Start(){
+			rigidBody = GetComponent<Rigidbody>();
+			player = GameObject.Find(PLAYER_NAME).transform;
+			playerWeapon = GameObject.Find(PLAYER_WEAPON_NAME).GetComponent<PlayerAbility.BasicLightWeapon>();
 		}
-	}
 
-	/// <summary>
-	/// Turn smoothly toward the player, accelerating up to a maximum speed as the enemy goes.
-	/// </summary>
-	protected virtual void ChasePlayer(){
-		transform.rotation = Quaternion.RotateTowards(transform.rotation,
-													  GetRotationToward(player),
-													  turnRate * Time.deltaTime);
-
-		rigidBody.AddForce(transform.right, ForceMode.Force);
-
-		if (rigidBody.velocity.magnitude > speed){
-			rigidBody.velocity = rigidBody.velocity.normalized * speed;
+		protected virtual void FixedUpdate(){
+			if (Vector3.Distance(player.position, transform.position) <= spotPlayerDistance){
+				ChasePlayer();
+			}
 		}
-	}
 
-	/// <summary>
-	/// Get an angle toward a target.
-	/// </summary>
-	/// <returns>The angle pointing toward the target.</returns>
-	/// <param name="target">The transform of the target.</param>
-	protected Quaternion GetRotationToward(Transform target){
-		Vector3 vectorRotation = (target.position - transform.position).normalized;
+		/// <summary>
+		/// Turn smoothly toward the player, accelerating up to a maximum speed as the enemy goes.
+		/// </summary>
+		protected virtual void ChasePlayer(){
+			transform.rotation = Quaternion.RotateTowards(transform.rotation,
+														  GetRotationToward(player),
+														  turnRate * Time.deltaTime);
 
-		float zRotation = Mathf.Atan2(vectorRotation.y, vectorRotation.x) * Mathf.Rad2Deg;
+			rigidBody.AddForce(transform.right, ForceMode.Force);
 
-		vectorRotation.z = zRotation;
-
-		return Quaternion.Euler(vectorRotation);
-	}
-
-	/// <summary>
-	/// What happens when this enemy is getting hit?
-	/// </summary>
-	protected virtual void OnTriggerStay(Collider other){
-		Debug.Log("OnTriggerStay() called");
-		if (other.gameObject.name.Contains(PLAYER_WEAPON_NAME)){
-			if (playerWeapon.Active) { health -= playerWeapon.Damage; }
-			if (health <= 0.0f){ Destroy(gameObject); }
+			if (rigidBody.velocity.magnitude > speed){
+				rigidBody.velocity = rigidBody.velocity.normalized * speed;
+			}
 		}
-	}
 
-	/// <summary>
-	/// What happens if this enemy contacts something?
-	/// </summary>
-	/// <param name="other">The collider of the thing the enemy hit.</param>
-	protected void OnTriggerEnter(Collider other){
-		if (other.gameObject.name.Contains(PLAYER_NAME)){
-			other.gameObject.GetComponent<VesselBehavior>().GotHit = true;
+		/// <summary>
+		/// Get an angle toward a target.
+		/// </summary>
+		/// <returns>The angle pointing toward the target.</returns>
+		/// <param name="target">The transform of the target.</param>
+		protected Quaternion GetRotationToward(Transform target){
+			Vector3 vectorRotation = (target.position - transform.position).normalized;
+
+			float zRotation = Mathf.Atan2(vectorRotation.y, vectorRotation.x) * Mathf.Rad2Deg;
+
+			vectorRotation.z = zRotation;
+
+			return Quaternion.Euler(vectorRotation);
+		}
+
+		/// <summary>
+		/// What happens when this enemy is getting hit?
+		/// </summary>
+		protected virtual void OnTriggerStay(Collider other){
+			Debug.Log("OnTriggerStay() called");
+			if (other.gameObject.name.Contains(PLAYER_WEAPON_NAME)){
+				if (playerWeapon.Active) { health -= playerWeapon.Damage; }
+				if (health <= 0.0f){ Destroy(gameObject); }
+			}
+		}
+
+		/// <summary>
+		/// What happens if this enemy contacts something?
+		/// </summary>
+		/// <param name="other">The collider of the thing the enemy hit.</param>
+		protected void OnTriggerEnter(Collider other){
+			if (other.gameObject.name.Contains(PLAYER_NAME)){
+				other.gameObject.GetComponent<VesselBehavior>().GotHit = true;
+			}
 		}
 	}
 }
