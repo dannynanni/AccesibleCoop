4ca3937f3b04e55c3de5abe583aa12d0d0a3dec3
Chris Chung <christopher0chung@gmail.com>
2016-09-28 16:31:32 -0400

    Made DesLevel1 and a random prefab spawning script.

--- Prototype - Sub - Phase 3/Assets/Scripts/SceneLoader.cs
+++ Prototype - Sub - Phase 3/Assets/Scripts/SceneLoader.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class SceneLoader : MonoBehaviour {
 
 	void Awake(){
-		SceneManager.LoadScene("Test level 2", LoadSceneMode.Additive);
+		SceneManager.LoadScene("DesLevel1", LoadSceneMode.Additive);
         SceneManager.LoadScene("Kraken", LoadSceneMode.Additive);
 
     }
---
+++ Prototype - Sub - Phase 3/Assets/TreasureSpawnScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreasureSpawnScript : MonoBehaviour {
+
+    public GameObject treasure;
+
+    public int numTreasure;
+
+    public float xMin;
+    public float xMax;
+
+    public float yMin;
+    public float yMax;
+
+	// Use this for initialization
+	void Start () {
+	
+        for (int i = 0; i < numTreasure; i++)
+        {
+            Instantiate(treasure, new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0), Quaternion.identity);
+        }
+
+	}
+	
+}
