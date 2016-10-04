79a2aa5e803b9dc30d4717c0b6d3c7ee9e8597ac
Chris Chung <christopher0chung@gmail.com>
2016-10-03 20:23:13 -0400

    Updated camera that looks up, clouds, and snow.

--- Prototype - Climbing - Phase 1/Assets/CameraManagerScript.cs
+++ Prototype - Climbing - Phase 1/Assets/CameraManagerScript.cs
@@ -17,8 +17,8 @@ public class CameraManagerScript : MonoBehaviour {
 
     private float cameraAng;
     private float targetAng;
-
-    public float camDist;
+    private float distToPlayer;
+    private float camDist;
 
 	// Use this for initialization
 	void Start () {
@@ -34,12 +34,15 @@ public class CameraManagerScript : MonoBehaviour {
         indexNum = findFirstPointAbove();
         Debug.Log(indexNum);
         lerpFloat = interpolationFloat(indexNum);
-        transform.position = Vector3.Lerp(transform.position, Vector3.Lerp(spinePoints[indexNum], spinePoints[indexNum + 1], lerpFloat), .05f);
+        Vector3 evenPos = Vector3.Lerp(transform.position, Vector3.Lerp(spinePoints[indexNum], spinePoints[indexNum + 1], lerpFloat), .05f);
+        transform.position = evenPos - Vector3.up * .15f;
 
         targetAng = calcTargetAng();
         cameraAng = Mathf.Lerp(cameraAng, targetAng, .05f);
         transform.rotation = Quaternion.Euler(0, cameraAng, 0);
 
+        distToPlayer = distToPlayerCalc(transform.position, targetPlayer.transform.position);
+        camDist = distToPlayer + 10;
         Camera.localPosition = new Vector3 (0, 0, camDist);
 
     }
@@ -65,6 +68,11 @@ public class CameraManagerScript : MonoBehaviour {
     private float calcTargetAng ()
     {
         return (Mathf.Rad2Deg * Mathf.Atan2(targetPlayer.transform.position.x - transform.position.x, targetPlayer.transform.position.z - transform.position.z));
+    }
+
+    private float distToPlayerCalc(Vector3 centerPos, Vector3 playerPos)
+    {
+        return Vector3.Distance(centerPos, playerPos); 
     }
 
     public void SetActivePlayer (GameObject activePlayer)
