7b501f71db4a9f6f8deffaf25aeaa6a8c4bf134d
Chris Chung <christopher0chung@gmail.com>
2016-10-08 16:53:17 -0400

    New boulder prefab, scripted instantiator,  and placeholder assets.

---
+++ Prototype - Climbing - Phase 2/Assets/ScriptedRandomInstantiator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptedRandomInstantiator : MonoBehaviour {
+
+    public GameObject spawnedObject;
+
+    public float xMin;
+    public float xMax;
+    public float yMin;
+    public float yMax;
+    public float tMin;
+    public float tMax;
+
+    private float timer;
+    private float tHold;
+
+    void Start ()
+    {
+        tHold = Random.Range(tMin, tMax);
+    }
+    void Update () {
+        timer += Time.deltaTime;
+        if (timer >= tHold)
+        {
+            Instantiate(spawnedObject, new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), 0), Quaternion.identity, transform);
+            timer = 0;
+        }
+    }
+}
