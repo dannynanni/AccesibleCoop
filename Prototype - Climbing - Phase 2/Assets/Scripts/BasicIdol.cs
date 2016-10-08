fbe804913ff30c78f088eb5315ddf508040760c4
tsarachan <tes353@nyu.edu>
2016-10-08 13:47:04 -0400

    Adding idols
    Adding scripts & prefabs for idols.

---
+++ Prototype - Climbing - Phase 2/Assets/Scripts/BasicIdol.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicIdol : MonoBehaviour {
+
+	public GameObject explosion;
+
+	public void CheckForDestruction(GameObject player)
+	{
+		if (player.tag == gameObject.tag){
+			Instantiate(explosion, transform.position, Quaternion.identity);
+			Destroy(gameObject);
+		}
+	}
+}
